508a24fffa1fb27b5c3d33a08e62fba2548c2314
keienWang <42377006+keienWang@users.noreply.github.com>
2024-05-22 16:07:57 +0800

    fix some error!

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import gettrace "get_eth_node/getTrace"
 
 func main() {
-	gettrace.GetTrace("", "0x68bf10465bdc4c8070fad2b1d24522d8185bfc19f012222ae39c032afe16d605")
+	gettrace.GetTrace()
 }
