96830e50440eb0ffe269132be941f3c4fa1c80d9
keienWang <42377006+keienWang@users.noreply.github.com>
2024-06-06 15:25:02 +0800

    Update main.go

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 
 // 将 gas price 转换为 Gwei
 func gasPriceInGwei(price *big.Int) *big.Float {
+
 	gwei := new(big.Float).Quo(new(big.Float).SetInt(price), big.NewFloat(1e9))
 	return gwei
 }
