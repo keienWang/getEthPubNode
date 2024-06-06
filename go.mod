010a25b223d49a1f72ae95c7b6f2ad6496d3e8fa
keienWang <42377006+keienWang@users.noreply.github.com>
2024-06-06 15:24:51 +0800

    update

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/bits-and-blooms/bitset v1.13.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-	getaccounts "get_eth_node/getAccounts"
-	"time"
+	getpeers "get_eth_node/getPeers"
+	"math/big"
 )
 
 func main() {
 
 	// trace.GetBlockTxTxR()
-	for {
-		getaccounts.GetAccount()
-		time.Sleep(1 * time.Second)
-	}
+	// for {
+	// 	getaccounts.GetAccount()
+	// 	time.Sleep(1 * time.Second)
+	// }
 
-	// getpeers.GetPeers()
+	getpeers.GetPeers()
 
+	// client, _ := ethclient.Dial("https://taiko.blockpi.network/v1/rpc/public")
+
+	// block, err := client.BlockByNumber(context.Background(), nil)
+	// if err != nil {
+	// 	log.Fatalf("Failed to get latest block: %v", err)
+	// }
+
+	// // 获取 gas price
+	// gasPrice, err := client.SuggestGasPrice(context.Background())
+	// if err != nil {
+	// 	log.Fatalf("Failed to get suggested gas price: %v", err)
+	// }
+
+	// // 打印结果
+	// fmt.Println("Current Gas Price: Gwei\n", gasPriceInGwei(gasPrice))
+	// fmt.Printf("Latest Block Number: %d\n", block.Number().Uint64())
+}
+
+// 将 gas price 转换为 Gwei
+func gasPriceInGwei(price *big.Int) *big.Float {
+	gwei := new(big.Float).Quo(new(big.Float).SetInt(price), big.NewFloat(1e9))
+	return gwei
 }
